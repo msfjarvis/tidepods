f6022cdc8f4427c771ba4036a59db4c8f072114e
Harsh Shandilya <me@msfjarvis.dev>
2020-07-24 15:12:46 +0530

    tests: update for stats format changes
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -2,6 +2,7 @@ package dev.msfjarvis
 
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.TestApplicationCall
 import io.ktor.server.testing.handleRequest
 import io.ktor.server.testing.setBody
 import io.ktor.server.testing.withTestApplication
@@ -24,35 +25,26 @@ class ApplicationTest {
         assertEquals("View recoded for https://msfjarvis.dev", response.content)
       }
       handleRequest(HttpMethod.Get, "/stats").apply {
-        assertEquals(HttpStatusCode.OK, response.status())
-        assertEquals(
-          """
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <title>Stats</title>
-            </head>
-            <body>
-              <h1>Stats</h1>
-              <table>
-                <thead>URL</thead>
-                <thead>Count</thead>
-                <tr>
-                  <td>https://msfjarvis.dev</td>
-                  <td>1</td>
-                </tr>
-              </table>
-            </body>
-          </html>
-          """.trimIndent(), response.content?.trimIndent()
-        )
+        verifyHtmlResponse()
+      }
+      handleRequest(HttpMethod.Get, "/stats?format=html").apply {
+        verifyHtmlResponse()
+      }
+      handleRequest(HttpMethod.Get, "/stats?format=badboi").apply {
+        assertEquals(HttpStatusCode.BadRequest, response.status())
+        assertEquals("Invalid format: badboi", response.content)
       }
       handleRequest(HttpMethod.Get, "/stats?format=json").apply {
         assertEquals(HttpStatusCode.OK, response.status())
         assertEquals("[{\"url\":\"https://msfjarvis.dev\",\"views\":1}]", response.content)
       }
       handleRequest(HttpMethod.Post, "/stats") {
-        setBody(Json(JsonConfiguration.Stable).stringify(Site.serializer().list, listOf(Site("https://msfjarvis.dev", 0))))
+        setBody(
+          Json(JsonConfiguration.Stable).stringify(
+            Site.serializer().list,
+            listOf(Site("https://msfjarvis.dev", 0))
+          )
+        )
       }.apply {
         assertEquals(HttpStatusCode.OK, response.status())
         assertEquals("Entered bulk data into stats DB", response.content)
@@ -62,5 +54,30 @@ class ApplicationTest {
         assertEquals("[{\"url\":\"https://msfjarvis.dev\",\"views\":0}]", response.content)
       }
     }
+  }
+
+  private fun TestApplicationCall.verifyHtmlResponse() {
+    assertEquals(HttpStatusCode.OK, response.status())
+    assertEquals(
+      """
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Stats</title>
+        </head>
+        <body>
+          <h1>Stats</h1>
+          <table>
+            <thead>URL</thead>
+            <thead>Count</thead>
+            <tr>
+              <td>https://msfjarvis.dev</td>
+              <td>1</td>
+            </tr>
+          </table>
+        </body>
+      </html>
+      """.trimIndent(), response.content?.trimIndent()
+    )
   }
 }
